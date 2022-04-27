aef64aaf2668105fa98fed73614addf343afdc24
Cesar Gonzalez <jugador409@hotmail.com>
2022-04-26 23:59:37 -0500

    Fix test

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -4,6 +4,6 @@ describe("Test para ExplorerService", () => {
     test("Requerimiento 1: Calcular todos los explorers en una mision", () => {
         const explorers = [{mission: "node"}]
         const explorersInNode = ExplorerService.filterByMission(explorers, "node")
-        expect(explorersInNode.length).toBe(4)
+        expect(explorersInNode.length).toBe(1)
     })
 })
