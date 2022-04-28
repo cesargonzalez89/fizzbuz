0f5136945f598d7b88ad42231a247b3dcda292cf
Cesar Gonzalez <jugador409@hotmail.com>
2022-04-28 00:36:00 -0500

    se agregan pruebas para el metodo readJsonFile de la clase Reader

---
+++ test/utils/Reader.test.js
@@ -0,0 +1,10 @@
+const Reader = require("./../../../fizzbuz/lib/utils/Reader");
+
+describe("Test para ExplorerService", () => {
+    test("Requerimiento 1: Calcular todos los explorers en una mision", () => {
+        const explorersRead = Reader.readJsonFile("explorers.json");
+        expect(explorersRead.length).toBe(15);
+    });
+});
+
+
