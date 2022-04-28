4b1f58f04af311e34a9697eb3b074e9d4fb419f8
Cesar Gonzalez <jugador409@hotmail.com>
2022-04-28 00:22:10 -0500

    Se agrega el codigo para el metodo estatico getAmountOfExplorersByMission

--- app.js
+++ app.js
@@ -1,16 +1,3 @@
-const fs = require("fs");
-
-// Part 1 Read json file ===========================
-const rawdata = fs.readFileSync("explorers.json");
-const explorers = JSON.parse(rawdata);//Lista de explorer del archivo json
-
-console.log("Hey! I'm HERE");
-//console.log(explorers)
-
-// Part 2: Get the quantity of explorers names in node
-const explorersInNode = explorers.filter((explorer) => explorer.mission == "node");
-console.log("Explorers en node: " + explorersInNode.length);
-
 // Part4: Get the explorer's usernames in Node
 const explorersInNodeToGetUsernames = explorers.filter((explorer) => explorer.mission == "node");
 const usernamesInNode = explorersInNodeToGetUsernames.map((explorer) => explorer.githubUsername);
