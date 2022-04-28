a7dec61651581805f9a7ab4b864a08b8e927df0b
Cesar Gonzalez <jugador409@hotmail.com>
2022-04-28 01:06:07 -0500

    Se agrega el codigo para el metodo  getExplorersUsernamesByMission y dos pruebas unitarias para el mismo

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -12,4 +12,16 @@ describe("Test para ExplorerService", () => {
         const explorersInNode = ExplorerService.getAmountOfExplorersByMission(explorers, "node");
         expect(explorersInNode).toBe(1);
     });
+
+    test("Requerimiento 3: Obtener los usuarios de los explorers de una mision especifica", () => {
+        const explorers = [{mission: "node", githubUsername: "ajolonauta1"}];
+        const explorersInNode = ExplorerService.getExplorersUsernamesByMission(explorers, "node");
+        expect(explorersInNode).toStrictEqual(["ajolonauta1"]);
+    });
+
+    test("Requerimiento 4: Obtener los usuarios de los explorers de una mision especifica", () => {
+        const explorers = [{mission: "node", githubUsername: "ajolonauta1"}, {mission: "java", githubUsername: "ajolonauta2"}, {mission: "node", githubUsername: "ajolonauta3"}];
+        const explorersInNode = ExplorerService.getExplorersUsernamesByMission(explorers, "node");
+        expect(explorersInNode).toStrictEqual(["ajolonauta1", "ajolonauta3"]);
+    });
 });
