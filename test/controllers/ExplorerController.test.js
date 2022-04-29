ef7daa0fece054a3ceb1c0383cb7d4afaf8f871c
Cesar Gonzalez <jugador409@hotmail.com>
2022-04-29 00:24:04 -0500

    se agrega prueba para el metodo getExplorersUsernamesByMission

--- test/controllers/ExplorerController.test.js
+++ test/controllers/ExplorerController.test.js
@@ -7,20 +7,19 @@ describe("Test para ExplorerController", () => {
     });
 
     test("Requerimiento 2: Calcular todos los explorers en la mision java", () => {
-        const explorersInNode = ExplorerController.getExplorersByMission("java");
-        expect(explorersInNode.length).toBe(5);
+        const explorersInJava = ExplorerController.getExplorersByMission("java");
+        expect(explorersInJava.length).toBe(5);
+    });
+
+    test("Requerimiento 3: Obtener los usuarios de los explorers de una mision especifica", () => {
+        const usersInNode = ExplorerController.getExplorersUsernamesByMission("node");
+        expect(usersInNode).toStrictEqual(["ajolonauta1", "ajolonauta2", "ajolonauta3", "ajolonauta4", "ajolonauta5", "ajolonauta11", "ajolonauta12", "ajolonauta13", "ajolonauta14", "ajolonauta15"]);
     });
 /*
     test("Requerimiento 2: Calcular la cantidad de explorers en una mision", () => {
         const explorers = [{mission: "node"}];
         const explorersInNode = ExplorerService.getAmountOfExplorersByMission(explorers, "node");
         expect(explorersInNode).toBe(1);
-    });
-
-    test("Requerimiento 3: Obtener los usuarios de los explorers de una mision especifica", () => {
-        const explorers = [{mission: "node", githubUsername: "ajolonauta1"}];
-        const explorersInNode = ExplorerService.getExplorersUsernamesByMission(explorers, "node");
-        expect(explorersInNode).toStrictEqual(["ajolonauta1"]);
     });
 
     test("Requerimiento 4: Obtener los usuarios de los explorers de una mision especifica", () => {
